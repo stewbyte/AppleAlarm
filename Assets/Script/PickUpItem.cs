98d6edb2e964e3f4c3f0f62c4ccc0bf7a6cf7128
stewbyte <xgenyofficial@gmail.com>
2025-06-01 01:32:23 +0200

    Implement PickUpItem

---
+++ Assets/Script/PickUpItem.cs
@@ -0,0 +1,30 @@
+using JetBrains.Rider.Unity.Editor;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private Transform objectGrabTransform;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Grab(Transform objectGrabTransform)
+    {
+        this.objectGrabTransform = objectGrabTransform;
+        rigidbody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (objectGrabTransform != null)
+        {
+            float lerpSpeed = 10f;
+            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabTransform.position, Time.deltaTime * lerpSpeed);
+            rigidbody.MovePosition(newPosition);
+        }
+    }
+}
